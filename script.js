088d34f7cca1df96e8adc8ff567a8be49d276bcc
OilyPotato819 <o.schneider@share.epsb.ca>
2021-11-02 14:48:54 -0600

    8:15

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Animation Basics
+// Simple Paint
 
 // Setup Canvas and Context
 let cnv = document.getElementById("myCanvas");
@@ -7,48 +7,61 @@ cnv.width = 600;
 cnv.height = 400;
 
 // Global Variables
-let rectX = 100;
-let rectSize = 80;
-let rectBlue = 255;
-let rectRed = 0;
-
-let dIsPressed = false;
+let mouseIsPressed = false;
+let mouseX, mouseY;
+let size = 5;
+let penColor = "black";
 
 // Main Program Loop (60 FPS)
 requestAnimationFrame(loop);
-function loop() {
-    // Update Variables
-    rectX += 1;
-    rectSize += Math.random() * 2 - 1;
-    rectBlue--;
-    rectRed++;
 
-    // Draw a background
-    ctx.fillStyle = "white"
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
-    
-    if (dIsPressed) {
-        // Draw a square
-        ctx.fillStyle = "rgb(" + rectRed + ", 0," + rectBlue + ")";
-        ctx.fillRect(rectX, 50, rectSize, rectSize);
+function loop() {
+    // Draw a circle if mouseIsPressed
+    if (mouseIsPressed) {
+        ctx.fillStyle = penColor;
+        ctx.beginPath();
+        ctx.arc(mouseX, mouseY, size, 0, 2 * Math.PI);
+        ctx.fill();
     }
 
     requestAnimationFrame(loop);
 }
 
 // Event Stuff
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+document.addEventListener("mousemove", mousemoveHandler);
 document.addEventListener("keydown", keydownHandler);
-document.addEventListener("keyup", keyupHandler);
 
-function keydownHandler() {
-    console.log(event.code)
-    if (event.code == "KeyD") {
-        dIsPressed = true;
-    }
+function mousedownHandler() {
+    mouseIsPressed = true;
 }
 
-function keyupHandler() {
-    if (event.code == "KeyD") {
-        dIsPressed = false;
+function mouseupHandler() {
+    mouseIsPressed = false;
+}
+
+function mousemoveHandler() {
+    let cnvRect = cnv.getBoundingClientRect();
+    mouseX = event.x - cnvRect.x;
+    mouseY = event.y - cnvRect.y;
+}
+
+function keydownHandler() {
+    if (event.code == "Space") {
+        // Draw a background
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, cnv.width, cnv.height);
+    } else if (event.code == "ArrowUp") {
+        size++;
+    } else if (event.code == "ArrowDown" && size > 1) {
+        console.log(size)
+        size--;
+    } else if (event.code == "Digit1") {
+        penColor = "red";
+    } else if (event.code == "Digit2") {
+        penColor = "green";
+    } else if (event.code == "Digit3") {
+        penColor = "blue";
     }
 }
