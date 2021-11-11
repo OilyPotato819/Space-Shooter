7f6e265a768795ec7c8f48f924a35729e5f57669
OilyPotato819 <o.schneider@share.epsb.ca>
2021-11-10 21:23:49 -0700

    almost done shooting
    fix resetting which bullet

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let bullet2X = shipX + 10;
 let bullet2Y = shipY - 20;
 let bullet1Shot = false;
 let bullet2Shot = false;
-let useBullet2 = 0;
+let whichBullet = 0;
 let dIsPressed = false;
 let aIsPressed = false;
 let wIsPressed = false;
@@ -23,8 +23,7 @@ let sIsPressed = false;
 let spaceIsPressed = false;
 let frameCount1 = 0;
 let frameCount2 = 0;
-let currentFrame = 0;
-let getCurrentFrame = false;
+let spaceFrameCount = 0;
 
 // Document Event Stuff
 document.addEventListener("keydown", keydownHandler);
@@ -45,9 +44,6 @@ function keydownHandler() {
     }
     if (event.code == "Space") {
         spaceIsPressed = true;
-        if (useBullet2 == 1) {
-            useBullet2++;
-        }
     }
 }
 
@@ -66,8 +62,8 @@ function keyupHandler() {
     }
     if (event.code == "Space") {
         spaceIsPressed = false;
-        if (useBullet2 == 0 && bullet1Shot) {
-            useBullet2++;
+        if (whichBullet == 1) {
+            whichBullet++;
         }
     }
 }
@@ -76,10 +72,33 @@ function keyupHandler() {
 requestAnimationFrame(loop);
 
 function loop() {
-    console.log(useBullet2)
+    console.log(whichBullet)
     // Background
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    // Decide which bullet to shoot
+    if (spaceIsPressed) {
+        spaceFrameCount++;
+        if (spaceFrameCount == 30) {
+            whichBullet++;
+        } else if (spaceFrameCount == 60) {
+            whichBullet++;
+            spaceFrameCount = 0;
+        } else if (whichBullet == 0) {
+            whichBullet++;
+        } else if (whichBullet == 2) {
+            whichBullet++;
+        }
+    }
+
+    if (whichBullet == 1) {
+        bullet1Shot = true;
+    }
+
+    if (whichBullet == 3) {
+        bullet2Shot = true;
+    }
 
     // Bullet1
     if (bullet1Shot) {
@@ -88,22 +107,12 @@ function loop() {
         bullet1Y -= 10;
 
         frameCount1++;
-        if (frameCount1 == 30 && spaceIsPressed) {
-            bullet2Shot = true;
-            bullet2X = shipX + 10;
-            bullet2Y = shipY - 20;
-        }
         if (frameCount1 == 60) {
             bullet1Shot = false;
             frameCount1 = 0;
             bullet1X = shipX + 10;
             bullet1Y = shipY - 20;
-            useBullet2 = 0;
         }
-    }
-
-    if (spaceIsPressed == true) {
-        bullet1Shot = true;
     }
 
     if (!bullet1Shot) {
@@ -112,9 +121,6 @@ function loop() {
     }
 
     // // Bullet2
-    if (useBullet2 == 2) {
-        bullet2Shot = true;
-    }
     if (bullet2Shot) {
         ctx.fillStyle = "blue";
         ctx.fillRect(bullet2X, bullet2Y, 5, 20);
@@ -126,7 +132,6 @@ function loop() {
             frameCount2 = 0;
             bullet2X = shipX + 10;
             bullet2Y = shipY - 20;
-            useBullet2 = 0;
         }
     }
 
